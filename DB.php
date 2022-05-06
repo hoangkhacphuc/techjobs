6c761be8a18ef248f32b44ea2d216d558a9189fd
= <=>
2022-05-06 20:21:37 +0700

    update db_insert trả về id data vừa insert

--- DB.php
+++ DB.php
@@ -23,6 +23,12 @@
         $str_values = substr($str_values, 0, -1);
         $query = "INSERT INTO `$table` ($str_keys) VALUES ($str_values)";
         $result = $GLOBALS['conn']->query($query);
+        if (!$result)
+            return false;
+
+        $result = mysqli_insert_id($GLOBALS['conn']);
+        $result = db_select($table, "`id` = $result");
+
         return $result;
     }
     
