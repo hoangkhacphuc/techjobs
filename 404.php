42437c00f85ece7e8bc2a1a6360be4d8b16caa95
= <=>
2022-05-10 18:49:56 +0700

    first commit

---
+++ 404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 ! ERROR</title>
+</head>
+<body>
+    <center>
+        <h1 style="font-size: 100px;">404 !</h1>
+        <a href="./" style="color: blue">Quay lại trang chủ</a>
+    </center>
+</body>
+</html>
